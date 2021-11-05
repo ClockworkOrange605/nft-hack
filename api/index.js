c35a40bf81c7c546cdc48e639048870a41328584
ak <kochkin.andrey@gmail.com>
2021-11-05 12:41:05 +0300

    [api] jwt auth

--- api/index.js
+++ api/index.js
@@ -1,37 +1,95 @@
-import fs from 'fs'
+// import { MongoClient } from 'mongodb'
+
 import express from 'express'
 import bodyParser from 'body-parser'
 
+import fs from 'fs'
+import path from 'path'
+
+import jwt from 'jsonwebtoken'
 import ethSigUtils from 'eth-sig-util'
 
 const port = 4000
+const secret = 'shhhhh'
+
 const api = express()
+
+console.log(process.env.ETHER_RPC)
+console.log(process.env.MONGO_URI)
+
+// const client = new MongoClient(process.env.MONGO_URI)
+
+// const dbName = 'test';
+
+// async function main() {
+//   // Use connect method to connect to the server
+//   await client.connect();
+//   console.log('Connected successfully to server');
+//   const db = client.db(dbName);
+//   const collection = db.collection('test');
+
+//   // the following code examples can be pasted here...
+//   // const insertResult = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
+//   // console.log('Inserted documents =>', insertResult);
+
+//   return 'done.';
+// }
+
+// main()
+//   .then(console.log)
+//   .catch(console.error)
+//   .finally(() => client.close());
 
 api.use(bodyParser.json())
 
+api.use('/preview', express.static('storage'))
+
 api.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send({
+    timestamp: Date.now()
+  })
 })
 
+/* Auth */
+
+function authMiddleware(req, res, next) {
+  const address = req.params.address
+  const token = req.headers['x-auth-token']
+
+  const { account } = jwt.verify(token, secret)
+  res.locals.account = account
+
+  if (account == address)
+    next()
+  else
+    res.status(403).send({ error: 'Invalid token' })
+}
 
 api.post('/auth/:address', (req, res) => {
-  console.log('POST', '/auth/:address', req.params, req.query, req.body)
+  console.log('POST', '/auth/:address')
 
   const address = req.params.address
   const signature = req.body.signature
-  const message = `${address}@crcode`
 
+  const message = `${address}@crcode`
   const account = ethSigUtils.recoverPersonalSignature({ data: message, sig: signature })
 
-  console.log({ address, signature, message, account })
+  const token = jwt.sign({ account, signature }, secret)
+  console.log(token)
 
   if (account == address)
-    res.send({ status: 'ok' })
+    res.send({ account, token })
   else
     res.status(403).send({ error: 'Invalid signature' })
 })
 
+api.get('/auth/:address/check', authMiddleware, async (req, res) => {
+  console.log('GET', '/auth/:address/check', req.headers)
 
+  res.send(res.locals)
+})
+
+/* Editor */
 api.get('/test', async (req, res) => {
   const address = '0x9f45deB282DA4AA19E4965E3483DCA19D93CaE01'
   const project = '01'
@@ -60,9 +118,10 @@ api.get('/test2', async (req, res) => {
   const project = '01'
   const filename = 'index.html'
 
-  const path = `./storage/${address}/${project}/source/${filename}`
+  // const path = `./storage/${address}/${project}/source/${filename}`
+  const filePath = path.resolve(`./storage/${address}/${filename}`)
 
-  const file = fs.readFileSync(path, 'utf-8')
+  const file = fs.readFileSync(filePath, 'utf-8')
 
   res.send(file)
 })
